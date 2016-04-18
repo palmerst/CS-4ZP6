a4ff1042cadbcbd2152a850cbdfcff3e9e6d07fa
chao56627561 <chao56627561@gmail.com>
2016-04-18 14:44:56 -0400

    finished 9 basic test case
    8 passed 1 failed

--- Automated Testing/make/src/UnitTest.cpp
+++ Automated Testing/make/src/UnitTest.cpp
@@ -34,9 +34,37 @@ class CollisionTest : public testing::Test {
   cpBody* heroBody;
 };
 
-TEST_F(InputTest, MoveLeft) {
+//stop10s stop1s
+//velocity.y=0 velocity.x=0
+TEST_F(InputTest, StopStop) {
+  
+  for(int i = 0; i < 60*10; i++){
+    st->processContinuousInput();
+    st->updateEnvironment(1.0f/60.0f);
+  }
+  
+  float vx1 = cpBodyGetVelocity(heroBody).x;
+  EXPECT_EQ(vx1, 0.0f);
+  
+  st->processKB(GLFW_KEY_A, 0, GLFW_RELEASE, 0);
+  st->processKB(GLFW_KEY_D, 0, GLFW_RELEASE, 0);
   
   for(int i = 0; i < 60; i++){
+    st->processContinuousInput();
+    st->updateEnvironment(1.0f/60.0f);
+  }
+  
+  float vx2 = cpBodyGetVelocity(heroBody).x;
+  
+  EXPECT_EQ(vx2, 0.0f);
+  
+}
+
+//stop 10s left 1s
+//velocity.y=0 velocity.x=decress
+TEST_F(InputTest, MoveLeft) {
+  
+  for(int i = 0; i < 60*10; i++){
     st->processContinuousInput();
     st->updateEnvironment(1.0f/60.0f);
   }
@@ -46,7 +74,7 @@ TEST_F(InputTest, MoveLeft) {
   
   st->processKB(GLFW_KEY_A, 0, GLFW_PRESS, 0);
   
-  for(int i = 0; i < 5*60; i++){
+  for(int i = 0; i < 60; i++){
     st->processContinuousInput();
     st->updateEnvironment(1.0f/60.0f);
   }
@@ -57,10 +85,11 @@ TEST_F(InputTest, MoveLeft) {
   
 }
 
-
+//stop 10s right 1s
+//velocity.y=0 velocity.x=increase
 TEST_F(InputTest, MoveRight) {
   
-  for(int i = 0; i < 60; i++){
+  for(int i = 0; i < 60*10; i++){
     st->processContinuousInput();
     st->updateEnvironment(1.0f/60.0f);
   }
@@ -70,7 +99,7 @@ TEST_F(InputTest, MoveRight) {
   
   st->processKB(GLFW_KEY_D, 0, GLFW_PRESS, 0);
   
-  for(int i = 0; i < 5*60; i++){
+  for(int i = 0; i < 60; i++){
     st->processContinuousInput();
     st->updateEnvironment(1.0f/60.0f);
   }
@@ -80,6 +109,162 @@ TEST_F(InputTest, MoveRight) {
   EXPECT_GT(vx2, 0.0f);
   
 }
+
+//left 10s stop 1s
+//velocity.y=0 velocity.x=0
+TEST_F(InputTest, LeftStop) {
+  
+  for(int i = 0; i < 60*10; i++){
+	st->processKB(GLFW_KEY_A, 0, GLFW_PRESS, 0);
+    st->processContinuousInput();
+    st->updateEnvironment(1.0f/60.0f);
+  }
+  
+  float vx1 = cpBodyGetVelocity(heroBody).x;
+  EXPECT_LT(vx1, 0.0f);
+  
+  st->processKB(GLFW_KEY_A, 0, GLFW_RELEASE, 0);
+  
+  for(int i = 0; i < 60; i++){
+    st->processContinuousInput();
+    st->updateEnvironment(1.0f/60.0f);
+  }
+  
+  float vx2 = cpBodyGetVelocity(heroBody).x;
+  
+  EXPECT_EQ(vx2, 0.0f);
+  
+}
+
+
+//left 10s left 1s
+//velocity.y=0 velocity.x=unchanged
+TEST_F(InputTest, LeftLeft) {
+  
+  for(int i = 0; i < 60*10; i++){
+	st->processKB(GLFW_KEY_A, 0, GLFW_PRESS, 0);
+    st->processContinuousInput();
+    st->updateEnvironment(1.0f/60.0f);
+  }
+  
+  float vx1 = cpBodyGetVelocity(heroBody).x;
+  EXPECT_LT(vx1, 0.0f);
+    
+  for(int i = 0; i < 60; i++){
+	st->processKB(GLFW_KEY_A, 0, GLFW_PRESS, 0);
+    st->processContinuousInput();
+    st->updateEnvironment(1.0f/60.0f);
+  }
+  
+  float vx2 = cpBodyGetVelocity(heroBody).x;
+  
+  EXPECT_EQ(vx2, vx1);
+  
+}
+
+//left 10 right 1
+//velocity.y=0 velocity.x=increse
+TEST_F(InputTest, LeftRight) {
+  
+  for(int i = 0; i < 60*10; i++){
+	st->processKB(GLFW_KEY_A, 0, GLFW_PRESS, 0);
+    st->processContinuousInput();
+    st->updateEnvironment(1.0f/60.0f);
+  }
+  
+  float vx1 = cpBodyGetVelocity(heroBody).x;
+  EXPECT_LT(vx1, 0.0f);
+    
+  for(int i = 0; i < 60; i++){
+	st->processKB(GLFW_KEY_D, 0, GLFW_PRESS, 0);
+    st->processContinuousInput();
+    st->updateEnvironment(1.0f/60.0f);
+  }
+  
+  float vx2 = cpBodyGetVelocity(heroBody).x;
+  
+  EXPECT_GT(vx2, vx1);
+  
+}
+
+//right 10 stop 1
+//velocity.y=0 velocity.x=0
+TEST_F(InputTest, RightStop) {
+  
+  for(int i = 0; i < 60*10; i++){
+	st->processKB(GLFW_KEY_D, 0, GLFW_PRESS, 0);
+    st->processContinuousInput();
+    st->updateEnvironment(1.0f/60.0f);
+  }
+  
+  float vx1 = cpBodyGetVelocity(heroBody).x;
+  EXPECT_GT(vx1, 0.0f);
+    
+  st->processKB(GLFW_KEY_D, 0, GLFW_RELEASE, 0);
+  
+  for(int i = 0; i < 60; i++){
+    st->processContinuousInput();
+    st->updateEnvironment(1.0f/60.0f);
+  }
+  
+  float vx2 = cpBodyGetVelocity(heroBody).x;
+  
+  EXPECT_EQ(vx2, 0.0f);
+  
+}
+
+
+//right 10 left 1
+//velocity.y=0 velocity.x=decress
+TEST_F(InputTest, RightLeft) {
+  
+  for(int i = 0; i < 60*10; i++){
+	st->processKB(GLFW_KEY_D, 0, GLFW_PRESS, 0);
+    st->processContinuousInput();
+    st->updateEnvironment(1.0f/60.0f);
+  }
+  
+  float vx1 = cpBodyGetVelocity(heroBody).x;
+  EXPECT_GT(vx1, 0.0f);
+  
+  for(int i = 0; i < 60; i++){
+	st->processKB(GLFW_KEY_A, 0, GLFW_PRESS, 0);
+    st->processContinuousInput();
+    st->updateEnvironment(1.0f/60.0f);
+  }
+  
+  float vx2 = cpBodyGetVelocity(heroBody).x;
+  
+  EXPECT_LT(vx2, vx1);
+  
+}
+
+
+//right 10s right 1s
+//velocity.y=0 velocity.x=unchanged
+TEST_F(InputTest, RightRight) {
+  
+  for(int i = 0; i < 60*10; i++){
+	st->processKB(GLFW_KEY_D, 0, GLFW_PRESS, 0);
+    st->processContinuousInput();
+    st->updateEnvironment(1.0f/60.0f);
+  }
+  
+  float vx1 = cpBodyGetVelocity(heroBody).x;
+  EXPECT_GT(vx1, 0.0f);
+  
+  for(int i = 0; i < 60; i++){
+	st->processKB(GLFW_KEY_D, 0, GLFW_PRESS, 0);
+    st->processContinuousInput();
+    st->updateEnvironment(1.0f/60.0f);
+  }
+  
+  float vx2 = cpBodyGetVelocity(heroBody).x;
+  
+  EXPECT_EQ(vx2, vx1);
+  
+}
+
 
 // Tests platform support.
 TEST_F(CollisionTest, SupportedByPlatform) {
